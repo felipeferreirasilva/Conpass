865989a9f8d4ac855877935ee06650ab03d46e56
Felipe Silva <felipeferreirasilva@gmail.com>
2019-03-30 14:16:24 -0300

    Updates

---
+++ precache-manifest.b4f6f019fc496f2b4a4524597cbfae88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a48f912af32345e4bee8",
+    "url": "/Conpass/static/js/runtime~main.5745bc04.js"
+  },
+  {
+    "revision": "7c49d28ffbd98b39a9d4",
+    "url": "/Conpass/static/js/main.1c39054a.chunk.js"
+  },
+  {
+    "revision": "f82413e62d5b2d67a016",
+    "url": "/Conpass/static/js/2.01d19aa5.chunk.js"
+  },
+  {
+    "revision": "7c49d28ffbd98b39a9d4",
+    "url": "/Conpass/static/css/main.fe5be633.chunk.css"
+  },
+  {
+    "revision": "f82413e62d5b2d67a016",
+    "url": "/Conpass/static/css/2.0731e648.chunk.css"
+  },
+  {
+    "revision": "3bc1774fd3a418e7ed5224674be04433",
+    "url": "/Conpass/index.html"
+  }
+];
