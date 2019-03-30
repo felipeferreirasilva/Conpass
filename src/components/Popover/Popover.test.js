dae46404da25fdc8de3fe87d4f74dcab67e2ed61
Felipe Silva <felipeferreirasilva@gmail.com>
2019-03-30 12:51:51 -0300

    Edit Popover Test

---
+++ src/components/Popover/Popover.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+import Popover from '../Popover/Popover'
+
+// Importa compoenents do Bootstrap
+import PopoverBs from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+
+describe('<Popover />', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Popover />)
+    })
+
+    it('renders 1 Popover', () => {
+        expect(wrapper.find('#popover').length).toBe(1);
+    })
+
+})
